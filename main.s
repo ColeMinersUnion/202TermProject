53209cd4ebe163e4b3814c1905f346df3a022d2b
cwn15 <156032055+cwn15@users.noreply.github.com>
2024-04-16 14:06:16 -0400

    added seven seg and direction output

--- main.s
+++ main.s
@@ -30,6 +30,18 @@
 				
 __main	PROC
 	
+	;set a8, a9, a11, a12, a14, a15 as output
+	LDR r0, =RCC_BASE
+	LDR r1, [r0, #RCC_AHB2ENR]
+	ORR r1, #0x00000001			;set
+	STR r1, [r0, #RCC_AHB2ENR]
+	
+	LDR r0, =GPIOA_BASE
+	LDR r1, [r0, #GPIO_MODER]
+	ORR r1, #0x0000DB00	;set
+	STR r1, [r0, #GPIO_MODER]
+	
+	
 	;
 	;Variables used by the rest of the program
 	MOV r5, #0x00000000
@@ -37,13 +49,14 @@ __main	PROC
 
 	;using r7 as my working register
 	;r7 now stores the current floor
-while AND r7, r5, #0x000F0000
+	
+while AND r7, r5, #0x000F0000		;isolates for just current floor on 7
 	; check if the current floor is a destination
 	; or if it is called in the direction
 	AND r8, r5, #0x00000001
 	CMP r8, #0x1
 	BNE downFlags
-	BEQ upFlags
+	B upFlags
 midWhile
 	;flags set onto r9
 	LSR r7, #0x10
@@ -58,10 +71,47 @@ midWhile
 
 	CMP r8, #0x1
 	BNE downFloor
-	BEQ upFloor
+	B upFloor
 
 	;Update LEDs and character display here
-	B while
+	;get floor and move onto the appropriate pins A8, A9, A11, A12
+	;current floor is stored on r7
+seven 
+	LDR r0, =GPIOA_BASE
+	LDR r1, [r0, #GPIO_ODR]
+	ORR r2, r7, #0x00000003	;store first two bits of interest
+	LSR r2, #0x8			;move from bits 1,0 to bits 8,9
+	ORR r1, r2				;store bits onto 8,9 of r1
+	AND r2, #0x0			;reclear
+	ORR r2, r7, #0x0000000C	;store second two bits of interest
+	LSR r2, #0x9			;move bits 2,3 to 11,12
+	ORR r1, r2				;store bits onto 11,12 of r1
+	STR r1, [r0, #GPIO_ODR]	;store onto odr
+	BX LR
+	;this should update the seven segment display
+	
+direction
+	;direction lights are a14, a15
+	;r8 0x0 is down, r8 0x1 is up
+	LDR r0, =GPIOA_BASE
+	LDR r1, [r0, #GPIO_ODR]
+	CMP r8, #0x0
+	BEQ downlight
+	CMP r8, #0x1
+	BEQ uplight
+	
+downlight
+	ORR r1, #0x00008000
+	STR r1, [r0, #GPIO_ODR]
+	BX LR
+	
+uplight
+	ORR r1, #0x00004000
+	STR r1, [r0, #GPIO_ODR]
+	BX LR
+	
+	
+	
 downFlags
 	AND r9, #0x00000000
 	AND r9, r6, #0xF0
@@ -74,16 +124,19 @@ upFlags
 	B midWhile	
 
 openSesame
+	BL seven
 	BL doorOpen
 	BL keypad
 	BL doorClose
+	;where after here?
+	;B while??
 	
 downFloor
 	BL downloop
 	;reset floor
 	AND r5, #0xFFF0FFFF
 	LSL r7, #0x09
-	OR r5, r5, r7
+	ORR r5, r5, r7
 	;turn on next floor
 	;turn off previous destination and previous calls
 	
