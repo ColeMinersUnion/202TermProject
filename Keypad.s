c2430b6b819caf2bc6f63fd9a7460335f2be4f83
SawyerMervis <156098417+SawyerMervis@users.noreply.github.com>
2024-04-16 20:46:56 -0400

    Functional Keypad.s

--- Keypad.s
+++ Keypad.s
@@ -1,14 +1,10 @@
-	;INCLUDE core_cm4_constants.s		; Load Constant Definitions
 	INCLUDE stm32l476xx_constants.s
-		
-	IMPORT USART2_Write
-	IMPORT UART2_Init
 	
 	AREA    Keypad, CODE, READONLY
 	EXPORT	Keypad				; make __main visible to linker
 	
 
-	; r0 will contain pressed value (* = E, # = F)
+	; Corresponding flag will be updated after completion
 	; Pin map: GPIOC(0, 1, 2, 3) as output, GPIOB(1, 2, 3, 4) as input
 keypad PROC
 	
@@ -79,7 +75,7 @@ keypad PROC
 	AND r1, #0x1E
 	CMP r1, #0x1E
 	BNE checkrow4
-	BX LR  ; ellse exit
+	BX LR  ; else exit
 	
 wait	; Wait until col becomes high
 	;LDR r7, =GPIOB_BASE
@@ -87,142 +83,134 @@ wait	; Wait until col becomes high
 	AND r1, r1, r2	; mask col pin, will be 0 if low
 	CMP r1, #0x0		; if col low, loop
 	BEQ wait
-	;BL UART2_Init
-	MOV r1, #1    ; Second argument
-	BL USART2_Write
+	LDR r0, =GPIOC_BASE    ; Reset all rows
+	LDR r1, [r0, #GPIO_ODR]
+	BIC r1, #0xF
+	STR r1, [r0, #GPIO_ODR]
 	POP{LR}
 	BX LR
 		
 checkrow1
 
-		;LDR r7, =GPIOB_BASE
 		LDR r1, [r7, #GPIO_IDR] ; Load input
 		
 		MOV r2, #0x2 		; Compare to col 1
 		AND r3, r1, r2
-		CMP r3, #0x0		; If low, move char, wait for unpress, and display
-		;MOVEQ r5, #0x1
-		MOV r0, #0x1   ; First argument
+		CMP r3, #0x0		; If low, update flag, wait for unpress, exit interrupt
+		ORREQ r5, #0x1000    ; Update floor 1 flag
 		BEQ wait
 		
 		MOV r2, #0x4		; Compare to col 2
 		AND r3, r1, r2
-		CMP r3, #0x0		; If low, move char, wait for unpress, and display
-		MOVEQ r0, #0x2
+		CMP r3, #0x0		
+		ORREQ r5, #0x2000   ; Update floor 2 flag
 		BEQ wait
 		
 		MOV r2, #0x8
 		AND r3, r1, r2
 		CMP r3, #0x0		; Compare to col 3
-		MOVEQ r0, #0x3
-		BEQ wait			; If low, move char, wait for unpress, and display
+		ORREQ r5, #0x4000   ; Update floor 3 flag
+		BEQ wait			
 		
 		MOV r2, #0x10
 		AND r3, r1, r2
 		CMP r3, #0x0		; Compare to col 4
-		MOVEQ r0, #0xA
-		BEQ wait			; If low, move char, wait for unpress, and display
+		ORREQ r5, #0x8000
+		BEQ wait			
 		
 		BX LR
 		
 checkrow2
 
-		;LDR r7, =GPIOB_BASE
 		LDR r1, [r7, #GPIO_IDR]
 		
 		MOV r2, #0x2
 		AND r3, r1, r2
 		CMP r3, #0x0
-		MOVEQ r0, #0x4
+		ORREQ r6, #0x1  ; Up flag floor 1
 		BEQ wait
 		
 		MOV r2, #0x4
 		AND r3, r1, r2
 		CMP r3, #0x0
-		MOVEQ r0, #0x5
+		ORREQ r6, #0x2 ; Up flag floor 2
 		BEQ wait
 		
 		MOV r2, #0x8
 		AND r3, r1, r2
 		CMP r3, #0x0
-		MOVEQ r0, #0x6
+		ORREQ r6, #0x4 ; Up flag floor 3
 		BEQ wait
 		
 		MOV r2, #0x10
 		AND r3, r1, r2
 		CMP r3, #0x0
-		MOVEQ r0, #0xB
+		ORREQ r6, #0x80 ; Down flag floor 4
 		BEQ wait
 		
 		BX LR
 		
 checkrow3
 
-		;LDR r7, =GPIOB_BASE
 		LDR r1, [r7, #GPIO_IDR] ; Load input
 		
 		MOV r2, #0x2 		; Compare to col 1
 		AND r3, r1, r2
-		CMP r3, #0x0		; If low, move char, wait for unpress, and display
-		MOVEQ r0, #0x7
+		CMP r3, #0x0		; If low, update flag, wait for unpress, exit interrupt
+		; Unassigned button
 		BEQ wait
 		
 		MOV r2, #0x4		; Compare to col 2
 		AND r3, r1, r2
-		CMP r3, #0x0		; If low, move char, wait for unpress, and display
-		MOVEQ r0, #0x8
+		CMP r3, #0x0		
+		ORREQ r6, #0x20     ; Down flag floor 2
 		BEQ wait
 		
 		MOV r2, #0x8
 		AND r3, r1, r2
 		CMP r3, #0x0		; Compare to col 3
-		MOVEQ r0, #0x9
-		BEQ wait			; If low, move char, wait for unpress, and display
+		ORREQ r6, #0x40     ; Down flag floor 3
+		BEQ wait			
 		
 		MOV r2, #0x10
 		AND r3, r1, r2
 		CMP r3, #0x0		; Compare to col 4
-		MOVEQ r0, #0xC
-		BEQ wait			; If low, move char, wait for unpress, and display
+		; Unassigned button
+		BEQ wait			
 		
 		BX LR
 		
 checkrow4
 
-		;LDR r7, =GPIOB_BASE
 		LDR r1, [r7, #GPIO_IDR] ; Load input
 		
 		MOV r2, #0x2 		; Compare to col 1
 		AND r3, r1, r2
-		CMP r3, #0x0		; If low, move char, wait for unpress, and display
-		MOVEQ r0, #0xE
+		CMP r3, #0x0		; If low, update flag, wait for unpress, exit interrupt
+		ORREQ r5, #0x10
 		BEQ wait
 		
 		MOV r2, #0x4		; Compare to col 2
 		AND r3, r1, r2
-		CMP r3, #0x0		; If low, move char, wait for unpress, and display
-		MOVEQ r0, #0x0
+		CMP r3, #0x0		
+		ORREQ r5, #0x20
 		BEQ wait
 		
 		MOV r2, #0x8
 		AND r3, r1, r2
 		CMP r3, #0x0		; Compare to col 3
-		MOVEQ r0, #0xF
-		BEQ wait			; If low, move char, wait for unpress, and display
+		; Unassigned currently, Panic
+		BEQ wait			
 		
 		MOV r2, #0x10
 		AND r3, r1, r2
 		CMP r3, #0x0		; Compare to col 4
-		MOVEQ r0, #0xD
-		BEQ wait			; If low, move char, wait for unpress, and display
+		; Unassigned currently, Admin
+		BEQ wait			
 		
 		BX LR
 	
 	ENDP
-	
-
-
-	
 		
 
 delay	PROC
